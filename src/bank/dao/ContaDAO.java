0365dc0fd732fc4f5dad7b383f39d039dcf31d89
Bruno Henrique Woisa <brunowoisa@hotmail.com>
2016-06-15 23:14:42 -0300

    ContaDAO
    Correção

--- src/bank/dao/ContaDAO.java
+++ src/bank/dao/ContaDAO.java
@@ -6,7 +6,7 @@ package bank.dao;
  */
 public class ContaDAO {
     private String stmtAdicionar = "INSERT INTO conta (id_tipo,saldo,limite,montante_minimo,deposito_minimo) values (?,?,?,?,?)";
-    private String stmtListar = "SELECT * FROM conta JOIN cliente ON cliente.id_conta = conta.id";
+    private String stmtListar = "SELECT * FROM conta JOIN cliente ON cliente.id_conta = conta.id WHERE conta.id=?";
     private String stmtAtualizar = "UPDATE conta set saldo=? WHERE id=?";
     private String stmtExcluir = "DELETE FROM conta WHERE id=?";
     private String stmtAtualizarVinculo = "UPDATE cliente set id_conta=? WHERE id=?";
